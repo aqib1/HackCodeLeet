043a99cf1347667a5196432c9a3e1bad8f0384dc
Aqib Javed <aqibbutt3078@gmail.com>
2020-01-06 14:12:26 +0500

    with streaming

--- Codefights/src/LandOfLogics/SumUpNumbers.java
+++ Codefights/src/LandOfLogics/SumUpNumbers.java
@@ -1,29 +1,32 @@
 package LandOfLogics;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Arrays;
 
 public class SumUpNumbers {
 
-	static int sumUpNumbers(String inputString) {
-		int sum = 0;
-		String number = "";
-		for (char c : inputString.toCharArray()) {
-			if (Character.isDigit(c)) {
-				number += c;
-			} else {
-				if (!number.isEmpty()) {
-					sum += Integer.parseInt(number);
-					number = "";
-				}
-			}
-		}
-		if (!number.isEmpty())
-			sum += Integer.parseInt(number);
-		return sum;
-	}
+//	static int sumUpNumbers(String inputString) {
+//		int sum = 0;
+//		String number = "";
+//		for (char c : inputString.toCharArray()) {
+//			if (Character.isDigit(c)) {
+//				number += c;
+//			} else {
+//				if (!number.isEmpty()) {
+//					sum += Integer.parseInt(number);
+//					number = "";
+//				}
+//			}
+//		}
+//		if (!number.isEmpty())
+//			sum += Integer.parseInt(number);
+//		return sum;
+//	}
 	
-
+	static int sumUpNumbers(String inputString) {
+		return Arrays.stream(inputString.split("\\D+"))
+				.filter(x-> !x.isEmpty())
+				.mapToInt(Integer::new).sum();
+	}
 	public static void main(String[] args) {
 		System.out.println(sumUpNumbers("2 apples, 12 oranges"));
 	}
