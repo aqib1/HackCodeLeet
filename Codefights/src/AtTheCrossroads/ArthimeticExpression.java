a654d3417c40559533ead42384cc0757aab04a59
Aqib Javed <aqibbutt3078@gmail.com>
2020-01-10 18:21:06 +0500

    Arthimatic expression algo

---
+++ Codefights/src/AtTheCrossroads/ArthimeticExpression.java
@@ -0,0 +1,12 @@
+package AtTheCrossroads;
+
+public class ArthimeticExpression {
+
+	static boolean arithmeticExpression(int a, int b, int c) {
+		return a + b == c || a * b == c || a - b == c || ((double)a / b) == c;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
