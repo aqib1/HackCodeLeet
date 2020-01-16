c69ca23e2041d00f23b922f0cfce271a65aa9c28
Aqib Javed <aqibbutt3078@gmail.com>
2020-01-16 18:13:41 +0500

    bitwise operators

---
+++ Codefights/src/CornersOf0sAnd1s/BitwiseComplement.java
@@ -0,0 +1,13 @@
+package CornersOf0sAnd1s;
+
+public class BitwiseComplement {
+
+	public static void main(String[] args) {
+		int value = 20;
+		System.out.println(Integer.toBinaryString(value));
+		System.out.println(Integer.toBinaryString(20 & 21));
+		System.out.println(Integer.toBinaryString(~21));
+
+	}
+
+}
