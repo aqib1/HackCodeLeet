2a9a24d774623dc1eecee1a927e5c5e430a2bdc3
Aqib Javed <aqibbutt3078@gmail.com>
2020-01-17 21:35:28 +0500

    commit bitwise complement,

--- Codefights/src/CornersOf0sAnd1s/BitwiseComplement.java
+++ Codefights/src/CornersOf0sAnd1s/BitwiseComplement.java
@@ -3,10 +3,16 @@ package CornersOf0sAnd1s;
 public class BitwiseComplement {
 
 	public static void main(String[] args) {
-		int value = 20;
+		int value = 6;
 		System.out.println(Integer.toBinaryString(value));
-		System.out.println(Integer.toBinaryString(20 & 21));
-		System.out.println(Integer.toBinaryString(~21));
+		//System.out.println(Integer.toBinaryString(20 & 21));
+		//System.out.println(Integer.toBinaryString(~21));
+		//System.out.println(20 | 21);
+//		System.out.println(value ^ 21);
+//		System.out.println(Integer.toBinaryString(value << 1));
+//		System.out.println(value << 1);
+		System.out.println(Integer.toBinaryString(value << 2));
+		System.out.println(value << 2);
 
 	}
 
