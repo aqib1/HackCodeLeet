bb8164760a1b372cbe29994ea8ee83996b7fcfae
Aqib Javed <aqibbutt3078@gmail.com>
2020-01-17 22:17:14 +0500

    find equals bit on most right

--- Codefights/src/CornersOf0sAnd1s/FindEqualBitOnMostRight.java
+++ Codefights/src/CornersOf0sAnd1s/FindEqualBitOnMostRight.java
@@ -2,7 +2,7 @@ package CornersOf0sAnd1s;
 
 public class FindEqualBitOnMostRight {
 	static int equalPairOfBits(int n, int m) {
-		return Integer.lowestOneBit(~(n^m));
+		return Integer.lowestOneBit(~(n^m)) ;
 	}
 	public static void main(String[] args) {
 
