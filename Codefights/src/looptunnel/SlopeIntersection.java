15773315b8a07a8c3be502b7f1ef512cbdf7a6c5
Aqib Javed <aqibbutt3078@gmail.com>
2020-01-30 02:24:48 +0500

    GCD slope intersection

--- Codefights/src/looptunnel/SlopeIntersection.java
+++ Codefights/src/looptunnel/SlopeIntersection.java
@@ -1,21 +1,13 @@
 package looptunnel;
 
+import java.math.BigInteger;
+
 public class SlopeIntersection {
 	static int countBlackCells(int n, int m) {
-
-		 return n + m + gcd(n,m) - 2;
+		int gcd = BigInteger.valueOf(n).gcd(BigInteger.valueOf(m)).intValue();
+		 return n + m + gcd - 2;
 }
 
-	static int gcd(int n, int m) {
-		int gcd = 0;
-		
-		for(int x=1; x<=n && x<=m; x++) {
-			if(x%n ==0 && x%m==0)
-				gcd = x;
-		}
-		
-		return gcd;
-	}
 	public static void main(String[] args) {
 
 	}
