01195f380d1eebb0527bdeb136d5039feb090fd0
Aqib Javed <aqibbutt3078@gmail.com>
2020-01-30 02:28:00 +0500

    Array fill

---
+++ Codefights/src/listForestEdge/MainClass.java
@@ -0,0 +1,17 @@
+package listForestEdge;
+
+import java.util.Arrays;
+
+public class MainClass {
+	static int[] createArray(int size) {
+		int []arr = new int[size];
+		Arrays.fill(arr, 1);
+		return arr;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
