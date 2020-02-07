26c68d42a8f0d2ef3802ca34da60d8ac4e14fc93
Aqib Javed <aqibbutt3078@gmail.com>
2020-02-07 21:15:30 +0500

    propernouncorrection algos

--- Codefights/src/bookmarket/ProperNounCorrection.java
+++ Codefights/src/bookmarket/ProperNounCorrection.java
@@ -1,9 +1,26 @@
 package bookmarket;
 
+import java.util.stream.Stream;
+
 public class ProperNounCorrection {
 
+	String properNounCorrection(String noun) {
+		StringBuilder sb = new StringBuilder(noun.length());
+
+		Stream.of(noun).forEach(x -> {
+			if (x.length() > 1) {
+				sb.append(x.substring(0, 1).toUpperCase()).append(x.substring(1).toLowerCase());
+				;
+			} else {
+				sb.append(x.toUpperCase());
+			}
+		});
+
+		return sb.toString();
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println();
 
 	}
 
