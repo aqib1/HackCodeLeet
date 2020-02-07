2e1e53a0d316c6e8adc779425884c274ca811d06
Aqib Javed <aqibbutt3078@gmail.com>
2020-02-07 21:05:56 +0500

    some algos

---
+++ Codefights/src/bookmarket/EncloseInBrackets.java
@@ -0,0 +1,14 @@
+package bookmarket;
+
+public class EncloseInBrackets {
+
+	String encloseInBrackets(String inputString) {
+		return "(" + inputString + ")";
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Codefights/src/bookmarket/ProperNounCorrection.java
@@ -0,0 +1,10 @@
+package bookmarket;
+
+public class ProperNounCorrection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
