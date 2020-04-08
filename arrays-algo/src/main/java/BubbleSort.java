3626d1521d8e43dcde7effd18021d66d2247eb73
Aqib Javed <aqibbutt3078@gmail.com>
2020-04-09 01:37:56 +0500

    initial check tests

--- arrays-algo/src/main/java/BubbleSort.java
+++ arrays-algo/src/main/java/BubbleSort.java
@@ -1,10 +1,16 @@
+import java.util.EmptyStackException;
+import java.util.Objects;
 
 public class BubbleSort {
 	public int[] bubbleSort(int[] array) {
+		if (Objects.isNull(array) || array.length == 0)
+			throw new EmptyStackException();
+		if (array.length == 1)
+			return array;
 		return null;
 	}
+
 	public static void main(String[] args) {
-		
 
 	}
 
--- arrays-algo/src/test/java/BubbleSortTest.java
+++ arrays-algo/src/test/java/BubbleSortTest.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Method;
+import java.util.EmptyStackException;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -7,11 +8,28 @@ import org.junit.Test;
 public class BubbleSortTest {
 
 	private BubbleSort bubbleSort;
+
 	@Before
 	public void init() {
 		bubbleSort = new BubbleSort();
 	}
-	
+
+	@Test
+	public void testBubbleSortForOneElement() {
+		int[] array = bubbleSort.bubbleSort(new int[] { 2 });
+		Assert.assertArrayEquals(array, new int[] { 2 });
+	}
+
+	@Test(expected = EmptyStackException.class)
+	public void testBubbleSortForNullArray() {
+		bubbleSort.bubbleSort(null);
+	}
+
+	@Test(expected = EmptyStackException.class)
+	public void testBubbleSortForEmptyArray() {
+		bubbleSort.bubbleSort(new int[] {});
+	}
+
 	@Test
 	public void testBubbleSortMethod() throws NoSuchMethodException, SecurityException {
 		String methodName = "bubbleSort";
