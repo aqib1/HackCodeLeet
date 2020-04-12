e07ff90ec2c9135d82f63f200ec8f3c1aae4f9f4
Aqib Javed <aqibbutt3078@gmail.com>
2020-04-13 01:01:16 +0500

    Merge algorithm ...

--- arrays-algo/src/main/java/MergeSort.java
+++ arrays-algo/src/main/java/MergeSort.java
@@ -1,4 +1,14 @@
+import java.util.EmptyStackException;
+import java.util.Objects;
 
 public class MergeSort {
 
+	public int[] mergeSort(int[] array) {
+		if(Objects.isNull(array) || array.length == 0)
+			throw new EmptyStackException();
+		if(array.length == 1)
+			return array;
+		
+		return null;
+	}
 }
--- arrays-algo/src/test/java/MergeSortTest.java
+++ arrays-algo/src/test/java/MergeSortTest.java
@@ -1,10 +1,40 @@
+import java.lang.reflect.Method;
+import java.util.EmptyStackException;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-public class MergeSortTest {
-	
 
+public class MergeSortTest {
+	private MergeSort mergeSort;
+
+	@Before
+	public void init() {
+		mergeSort = new MergeSort();
+	}
+
+	@Test(expected = EmptyStackException.class)
+	public void testMergeSortForNullArray() {
+		mergeSort.mergeSort(null);
+	}
+
+	@Test(expected = EmptyStackException.class)
+	public void testMergeSortForEmptyArray() {
+		mergeSort.mergeSort(new int[] {});
+	}
+	
 	@Test
-	public void testMergeSortMethodExists() {
-		
+	public void testMergeSortForOneElementArray() {
+		int [] array = mergeSort.mergeSort(new int[] {1});
+		Assert.assertArrayEquals(array, new int[] {1});
+	}
+	
+	@Test
+	public void testMergeSortMethodExists() throws NoSuchMethodException, SecurityException {
+		String methodName = "mergeSort";
+		Class<?> c = MergeSort.class;
+		Method method = c.getDeclaredMethod(methodName, int[].class);
+		Assert.assertNotNull(method);
 	}
 }
