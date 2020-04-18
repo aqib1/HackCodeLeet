450523de3df07f319f0977bdc61cdc2f84ec883d
Aqib Javed <aqibbutt3078@gmail.com>
2020-04-18 22:51:33 +0500

    add two number final commit

--- leetcode/src/main/java/leetcode/arrays/AddTwoNumbersWithListNode.java
+++ leetcode/src/main/java/leetcode/arrays/AddTwoNumbersWithListNode.java
@@ -1,5 +1,28 @@
 package leetcode.arrays;
 
+import java.util.EmptyStackException;
+import java.util.Objects;
+
 public class AddTwoNumbersWithListNode {
 
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		if (Objects.isNull(l1) || Objects.isNull(l2))
+			throw new EmptyStackException();
+		ListNode result = new ListNode(-1);
+		ListNode pointer = result;
+		int carry = 0;
+		while (!Objects.isNull(l1) || !Objects.isNull(l2)) {
+			int sum = !Objects.isNull(l1) && !Objects.isNull(l2) ? (l1.val + l2.val)
+					: !Objects.isNull(l1) ? l1.val : !Objects.isNull(l2) ? l2.val : -1;
+			sum += carry;
+			carry = sum / 10;
+			pointer.next = new ListNode(sum % 10);
+			pointer = pointer.next;
+			l1 = !Objects.isNull(l1) ? l1.next : l1;
+			l2 = !Objects.isNull(l2) ? l2.next : l2;
+		}
+		if (carry > 0)
+			pointer.next = new ListNode(carry);
+		return result.next;
+	}
 }
--- leetcode/src/test/java/leetcode/arrays/AddtwoNumbersWithListNodeTest.java
+++ leetcode/src/test/java/leetcode/arrays/AddtwoNumbersWithListNodeTest.java
@@ -1,5 +1,58 @@
 package leetcode.arrays;
 
+import java.lang.reflect.Method;
+import java.util.EmptyStackException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class AddtwoNumbersWithListNodeTest {
 
+	private AddTwoNumbersWithListNode addTwoNumbersWithListNode;
+
+	@Before
+	public void init() {
+		addTwoNumbersWithListNode = new AddTwoNumbersWithListNode();
+	}
+
+	@Test(expected = EmptyStackException.class)
+	public void testAddTwoNumbersNullListNode() {
+		addTwoNumbersWithListNode.addTwoNumbers(null, null);
+	}
+
+	@Test
+	public void testAddTwoNumbers() {
+		ListNode l1 = new ListNode(2);
+		l1.next = new ListNode(4);
+		l1.next.next = new ListNode(3);
+
+		ListNode l2 = new ListNode(5);
+		l2.next = new ListNode(6);
+		l2.next.next = new ListNode(4);
+
+		ListNode result = addTwoNumbersWithListNode.addTwoNumbers(l1, l2);
+
+		Assert.assertEquals(result.val, 7);
+		Assert.assertEquals(result.next.val, 0);
+		Assert.assertEquals(result.next.next.val, 8);
+	}
+
+	@Test
+	public void testAddTwoNumbers1() {
+		ListNode l1 = new ListNode(0);
+		ListNode l2 = new ListNode(7);
+		l2.next = new ListNode(3);
+		ListNode result = addTwoNumbersWithListNode.addTwoNumbers(l1, l2);
+		Assert.assertEquals(result.val, 7);
+		Assert.assertEquals(result.next.val, 3);
+	}
+
+	@Test
+	public void testAddTwoNumbersMethod() throws NoSuchMethodException, SecurityException {
+		String methodName = "addTwoNumbers";
+		Class<?> c = AddTwoNumbersWithListNode.class;
+		Method method = c.getDeclaredMethod(methodName, ListNode.class, ListNode.class);
+		Assert.assertNotNull(method);
+	}
 }
