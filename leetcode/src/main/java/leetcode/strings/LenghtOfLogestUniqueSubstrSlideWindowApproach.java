2ff995ed40feedd31901981b6ad8a9dec17a9ffb
Aqib Javed <aqibbutt3078@gmail.com>
2020-04-23 17:21:46 +0500

    sliding window approach to find maximum substring

--- leetcode/src/main/java/leetcode/strings/LenghtOfLogestUniqueSubstrSlideWindowApproach.java
+++ leetcode/src/main/java/leetcode/strings/LenghtOfLogestUniqueSubstrSlideWindowApproach.java
@@ -1,5 +1,36 @@
 package leetcode.strings;
 
+import java.util.Objects;
+
 public class LenghtOfLogestUniqueSubstrSlideWindowApproach {
 
+	/*
+	 * value -> aabc uniqueSubString(value)
+	 * 
+	 * - brute-force approach -> all possible subStrigs (unique one's) -> and return
+	 * the longest substring length O(n)3 with O(n) time complexity - Sliding window
+	 * Approach -> use two pointer like (i, j) == 0 which point two character's of
+	 * string (like a window) -> if character is unique then we will expand window
+	 * from and end else we reduce window from start -> each time when we have
+	 * unique character we cached max length so far -> Time complexity O(n) and
+	 * space complexity O(1) if we use fixed character array, and if we use set then
+	 * O(n)
+	 */
+	public int uniqueSubString(String value) {
+		if (Objects.isNull(value) || value.isEmpty())
+			return 0;
+		if (value.length() == 1)
+			return 1;
+		final int[] charCount = new int[123];
+		int x = 0, y = 0, n = value.length(), max = Integer.MIN_VALUE;
+		while (x < n && y < n) {
+			if (charCount[value.charAt(x)] < 1) {
+				charCount[value.charAt(x++)]++;
+				max = Math.max(max, x-y);
+			} else {
+				charCount[value.charAt(y++)]--;
+			}
+		}
+		return max;
+	}
 }
--- leetcode/src/test/java/leetcode/strings/LenghtOfLogestUniqueSubstrSlideWindowApproachTest.java
+++ leetcode/src/test/java/leetcode/strings/LenghtOfLogestUniqueSubstrSlideWindowApproachTest.java
@@ -1,5 +1,61 @@
 package leetcode.strings;
 
+import java.lang.reflect.Method;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class LenghtOfLogestUniqueSubstrSlideWindowApproachTest {
 
+	private LenghtOfLogestUniqueSubstrSlideWindowApproach approach;
+
+	@Before
+	public void init() {
+		approach = new LenghtOfLogestUniqueSubstrSlideWindowApproach();
+	}
+
+	@Test
+	public void testUniqueSubStringWithNullValue() {
+		int value = approach.uniqueSubString(null);
+		Assert.assertEquals(value, 0);
+	}
+
+	@Test
+	public void testUniqueSubStringWithEmptyValue() {
+		int value = approach.uniqueSubString("");
+		Assert.assertEquals(value, 0);
+	}
+
+	@Test
+	public void testUniqueSubStringWithSingleValue() {
+		int value = approach.uniqueSubString("a");
+		Assert.assertEquals(value, 1);
+	}
+
+	@Test
+	public void testUniqueSubStringMethod() throws NoSuchMethodException, SecurityException {
+		String methodName = "uniqueSubString";
+		Class<?> c = LenghtOfLogestUniqueSubstrSlideWindowApproach.class;
+		Method method = c.getDeclaredMethod(methodName, String.class);
+		Assert.assertNotNull(method);
+	}
+
+	@Test
+	public void testUniqueSubString() {
+		int value = approach.uniqueSubString("abb");
+		Assert.assertEquals(value, 2);
+	}
+
+	@Test
+	public void testUniqueSubString1() {
+		int value = approach.uniqueSubString("abcdefgh");
+		Assert.assertEquals(value, 8);
+	}
+	
+	@Test
+	public void testUniqueSubString2() {
+		int value = approach.uniqueSubString("bbbbbb");
+		Assert.assertEquals(value, 1);
+	}
 }
