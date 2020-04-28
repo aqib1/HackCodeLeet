78a8b8d497dab8db10da65d5783ca03834db2228
Aqib Javed <aqibbutt3078@gmail.com>
2020-04-29 02:55:28 +0500

    strings anagram (permutation) of each others

--- arrays-algo/src/main/java/generalbasicalgos/StringsAnagramAlgo.java
+++ arrays-algo/src/main/java/generalbasicalgos/StringsAnagramAlgo.java
@@ -1,5 +1,24 @@
 package generalbasicalgos;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class StringsAnagramAlgo {
 
+	private int[] chars = new int[256];
+
+	// Time complexity O(n) + O(n) = O(2n) which is O(n)
+	// Space complexity is O(1) as we have fixed size of chars array, 256 characters 
+	public boolean arePermutation(String s1, String s2) {
+		if (Objects.isNull(s1) || Objects.isNull(s2) || s1.isEmpty() || s2.isEmpty() || s1.length() != s2.length()) {
+			return false;
+		}
+		int limit = s1.length();
+		for (int x = 0; x < limit; x++) {
+			chars[s1.charAt(x)]++;
+			chars[s2.charAt(x)]++;
+		}
+		long count = Arrays.stream(chars).filter(x -> x == 2).count();
+		return count == limit;
+	}
 }
--- arrays-algo/src/test/java/generalbasicalgos/StringsAnagramAlgoTest.java
+++ arrays-algo/src/test/java/generalbasicalgos/StringsAnagramAlgoTest.java
@@ -1,5 +1,73 @@
 package generalbasicalgos;
 
+import java.lang.reflect.Method;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class StringsAnagramAlgoTest {
 
+	private StringsAnagramAlgo stringsAnagramAlgo;
+
+	@Before
+	public void init() {
+		stringsAnagramAlgo = new StringsAnagramAlgo();
+	}
+
+	@Test
+	public void arePermutationTest8() {
+		boolean result = stringsAnagramAlgo.arePermutation(null, null);
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void arePermutationTest7() {
+		boolean result = stringsAnagramAlgo.arePermutation(null, "bcba");
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void arePermutationTest6() {
+		boolean result = stringsAnagramAlgo.arePermutation("abc", null);
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void arePermutationTest5() {
+		boolean result = stringsAnagramAlgo.arePermutation("", "bcba");
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void arePermutationTest4() {
+		boolean result = stringsAnagramAlgo.arePermutation("abc", "");
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void arePermutationTest3() {
+		boolean result = stringsAnagramAlgo.arePermutation("abc", "bcba");
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void arePermutationTest2() {
+		boolean result = stringsAnagramAlgo.arePermutation("abc", "bcb");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void arePermutationTest1() {
+		boolean result = stringsAnagramAlgo.arePermutation("abc", "bca");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void arePermutationMethodTest() throws NoSuchMethodException, SecurityException {
+		String methodName = "arePermutation";
+		Class<?> c = StringsAnagramAlgo.class;
+		Method method = c.getDeclaredMethod(methodName, String.class, String.class);
+		Assert.assertNotNull(method);
+	}
 }
