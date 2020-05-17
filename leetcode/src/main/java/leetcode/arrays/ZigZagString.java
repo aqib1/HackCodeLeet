8ab50df315634fdbc952a794101f02e0190b9d2e
Aqib Javed <aqibbutt3078@gmail.com>
2020-05-18 03:03:48 +0500

    zigzag pattern

--- leetcode/src/main/java/leetcode/arrays/ZigZagString.java
+++ leetcode/src/main/java/leetcode/arrays/ZigZagString.java
@@ -1,14 +1,29 @@
 package leetcode.arrays;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class ZigZagString {
-
+	
 	public String convert(String s, int numRows) {
-		if(Objects.isNull(s) || s.isEmpty() || numRows < 1)
+		if (Objects.isNull(s) || s.isEmpty() || numRows < 1)
 			throw new IllegalArgumentException();
-		if(numRows == 1)
+		if (numRows == 1)
 			return s;
-		return s;
+		int limitRows = Math.min(numRows, s.length());
+		List<StringBuilder> rows = new ArrayList<>();
+		for(int x=0; x<limitRows; x++)
+			rows.add(new StringBuilder());
+		int currRow = 0;
+		boolean goingDown = false;
+		for(char c: s.toCharArray()) {
+			rows.get(currRow).append(c);
+			if(currRow >= numRows - 1 || currRow <= 0) goingDown = !goingDown;
+			currRow += goingDown ? 1:-1;
+		}
+		StringBuilder result = new StringBuilder();
+		for(StringBuilder sb: rows) result.append(sb);
+		return result.toString();
 	}
 }
--- leetcode/src/test/java/leetcode/arrays/ZigZagStringTest.java
+++ leetcode/src/test/java/leetcode/arrays/ZigZagStringTest.java
@@ -24,6 +24,12 @@ public class ZigZagStringTest {
 		zigZagString.convert("", 1);
 	}
 	
+	@Test
+	public void testConvert() {
+		String result = zigZagString.convert("PAYPALISHIRING", 3);
+		Assert.assertEquals("PAHNAPLSIIGYIR", result);
+	}
+	
 	@Test(expected = IllegalArgumentException.class)
 	public void testConvertWithNegitiveString() {
 		zigZagString.convert("a", -1);
