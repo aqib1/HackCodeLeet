9ea65e9985fb1cbcce12941437d2d7740b598600
Aqib Javed <aqibbutt3078@gmail.com>
2020-05-27 02:05:05 +0500

    Linkedlist has cycle

--- hackerrant-project/src/main/java/linkedlist/LinkedListCycleDetection.java
+++ hackerrant-project/src/main/java/linkedlist/LinkedListCycleDetection.java
@@ -1,9 +1,21 @@
 package linkedlist;
 
+import java.util.Objects;
+
 public class LinkedListCycleDetection {
-
+	// Using floyd's hare and tortoise approach
 	boolean hasCycle(SinglyLinkedListNode head) {
-
+		SinglyLinkedListNode hare = head;
+		SinglyLinkedListNode tortoise = head;
+		while (!Objects.isNull(hare) && !Objects.isNull(hare.next)) {
+			hare = hare.next.next;
+			tortoise = tortoise.next;
+			if (hare == tortoise) {
+				// System.out.println(1);
+				return true;
+			}
+		}
+		// System.out.println(0);
 		return false;
 	}
 }
--- hackerrant-project/src/test/java/linkedlist/LinkedListCycleDetectionTest.java
+++ hackerrant-project/src/test/java/linkedlist/LinkedListCycleDetectionTest.java
@@ -3,9 +3,24 @@ package linkedlist;
 import java.lang.reflect.Method;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class LinkedListCycleDetectionTest {
+	private LinkedListCycleDetection linkedListCycleDetection;
+
+	@Before
+	public void setUp() {
+		linkedListCycleDetection = new LinkedListCycleDetection();
+	}
+
+	@Test
+	public void testHasCycle() {
+		SinglyLinkedListNode li = new SinglyLinkedListNode(1);
+		li.next = new SinglyLinkedListNode(2);
+		li.next.next = li.next;
+		Assert.assertTrue(linkedListCycleDetection.hasCycle(li));
+	}
 
 	@Test
 	public void hasCycleMethodTest() throws NoSuchMethodException, SecurityException {
