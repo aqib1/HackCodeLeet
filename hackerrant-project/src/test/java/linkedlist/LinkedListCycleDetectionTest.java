bc0e8ea4a9745e9536fb20da180ec4463844a41d
Aqib Javed <aqibbutt3078@gmail.com>
2020-05-27 02:05:46 +0500

    negitive test case

--- hackerrant-project/src/test/java/linkedlist/LinkedListCycleDetectionTest.java
+++ hackerrant-project/src/test/java/linkedlist/LinkedListCycleDetectionTest.java
@@ -23,6 +23,14 @@ public class LinkedListCycleDetectionTest {
 	}
 
 	@Test
+	public void testHasCycle1() {
+		SinglyLinkedListNode li = new SinglyLinkedListNode(1);
+		li.next = new SinglyLinkedListNode(2);
+		li.next.next = new SinglyLinkedListNode(3);
+		Assert.assertFalse(linkedListCycleDetection.hasCycle(li));
+	}
+
+	@Test
 	public void hasCycleMethodTest() throws NoSuchMethodException, SecurityException {
 		String methodName = "hasCycle";
 		Class<?> c = LinkedListCycleDetection.class;
