5b10b5e583c445dfcdd907c62c71b47dc5b54da0
Aqib Javed <aqibbutt3078@gmail.com>
2020-06-27 08:17:05 +0500

    space, time complexity added

--- Time-Algoritms/src/main/java/algorithm/WaterTappingProblem.java
+++ Time-Algoritms/src/main/java/algorithm/WaterTappingProblem.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 public class WaterTappingProblem {
 
 	// [1,8,6,2,5,4,8,3,7] -> 49
+	// Time complexity O(n) and space complexity O(1)
 	public static int maxArea(int[] height) {
 		if (Objects.isNull(height) || height.length == 0)
 			return 0;
