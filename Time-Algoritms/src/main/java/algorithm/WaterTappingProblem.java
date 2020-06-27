69188f609ce67b6fd99d9ff739bb2c798f834ede
Aqib Javed <aqibbutt3078@gmail.com>
2020-06-27 08:16:27 +0500

    Water tapping problem time (5 minutes)

---
+++ Time-Algoritms/src/main/java/algorithm/WaterTappingProblem.java
@@ -0,0 +1,30 @@
+package algorithm;
+
+import java.util.Objects;
+
+public class WaterTappingProblem {
+
+	// [1,8,6,2,5,4,8,3,7] -> 49
+	public static int maxArea(int[] height) {
+		if (Objects.isNull(height) || height.length == 0)
+			return 0;
+
+		int i = 0;
+		int j = height.length - 1;
+		int maxArea = Integer.MIN_VALUE;
+		while (i < j) {
+			maxArea = Math.max(maxArea, Math.min(height[i], height[j]) * (j - i));
+			if (height[i] < height[j])
+				i++;
+			else
+				j--;
+		}
+
+		return maxArea;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(maxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
+	}
+
+}
