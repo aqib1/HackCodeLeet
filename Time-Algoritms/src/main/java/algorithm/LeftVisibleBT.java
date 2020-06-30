f9229d244614209768d48f353b43783348fd86fa
Aqib Javed <aqibbutt3078@gmail.com>
2020-07-01 03:03:59 +0500

    LeftVisibleBt and right visible BT

---
+++ Time-Algoritms/src/main/java/algorithm/LeftVisibleBT.java
@@ -0,0 +1,39 @@
+package algorithm;
+
+import java.util.List;
+
+public class LeftVisibleBT {
+
+	public static void main(String[] args) {
+
+	}
+
+	private int MAX_LEVEL = -1;
+
+	public void rightSideView(List<Integer> data, TreeNode root, int currentLevel) {
+		if (root == null)
+			return;
+
+		if (currentLevel > MAX_LEVEL) {
+			data.add(root.val);
+			MAX_LEVEL = currentLevel;
+		}
+
+		rightSideView(data, root.right, currentLevel + 1);
+		rightSideView(data, root.left, currentLevel + 1);
+	}
+
+//	public static void leftSideView(TreeNode root, int currentLevel) {
+//		if (root == null)
+//			return;
+//
+//		if (currentLevel > MAX_LEVEL) {
+//			System.out.println(root.val);
+//			MAX_LEVEL = currentLevel;
+//		}
+//
+//		leftSideView(root.left, currentLevel + 1);
+//		leftSideView(root.right, currentLevel + 1);
+//	}
+
+}
