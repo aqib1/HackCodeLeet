a11a6c1b89b3472dfb191530ed6864ecd397dd01
Aqib Javed <aqibbutt3078@gmail.com>
2020-07-02 16:09:32 +0500

    find from rotated array

--- Time-Algoritms/src/main/java/algorithm/FindFromRotatedArray.java
+++ Time-Algoritms/src/main/java/algorithm/FindFromRotatedArray.java
@@ -2,30 +2,42 @@ package algorithm;
 
 public class FindFromRotatedArray {
 	public static int search(int[] nums, int target) {
-		if (nums == null || nums.length == 0)
+		int pivot = pivot(nums, 0, nums.length - 1);
+		if (nums[pivot] == target)
+			return pivot;
+		if (pivot == -1)
+			return search(nums, 0, nums.length - 1, target);
+		if (target > nums[0])
+			return search(nums, 0, pivot - 1, target);
+		return search(nums, pivot + 1, nums.length - 1, target);
+	}
+
+	public static int search(int arr[], int first, int last, int target) {
+		if (first > last)
 			return -1;
-		if (nums.length == 1)
-			return nums[0] == target ? 0 : -1;
-		int index = -1;
-		int i = nums.length / 2;
-		if (nums[i] == target)
-			return i;
-		boolean leftVisit = //
-				nums[nums.length - 1] > nums[0] ? //
-						!(target >= nums[i]) : //
-						target >= nums[0] && target <= nums[i - 1];
-		int start = leftVisit ? 0 : i + 1;
-		int limit = leftVisit ? i : nums.length;
-		while (start < limit) {
-			if (target == nums[start])
-				return start;
-			start++;
-		}
-		return index;
+		int mid = (first + last) / 2;
+		if (arr[mid] == target)
+			return mid;
+		if (target >= arr[mid])
+			return search(arr, mid + 1, last, target);
+		return search(arr, first, mid - 1, target);
+	}
+
+	public static int pivot(int[] arr, int first, int last) {
+		if (first > last)
+			return -1;
+		if (first == last)
+			return first;
+		int mid = (last + first) / 2;
+		if (mid < last && arr[mid] > arr[mid + 1])
+			return mid;
+		if (arr[first] > arr[mid])
+			return pivot(arr, first, mid - 1);
+		return pivot(arr, mid + 1, last);
 	}
 
 	public static void main(String[] args) {
-		System.out.println(search(new int[] { 7, 8, 1, 2, 3, 4, 5, 6 }, 2));
+		System.out.println(search(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 2));
 	}
 
 }
