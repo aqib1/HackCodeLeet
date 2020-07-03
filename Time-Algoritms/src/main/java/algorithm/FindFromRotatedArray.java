02adfe9f814b4fa6386b79ef5a7bf941208341d3
Aqib Javed <aqibbutt3078@gmail.com>
2020-07-03 20:50:59 +0500

    clean pivot method

--- Time-Algoritms/src/main/java/algorithm/FindFromRotatedArray.java
+++ Time-Algoritms/src/main/java/algorithm/FindFromRotatedArray.java
@@ -3,9 +3,16 @@ package algorithm;
 public class FindFromRotatedArray {
 	// time complexity is Olog(n) and space is O(1)
 	public static int search(int[] nums, int target) {
-		int pivot = pivot(nums, 0, nums.length - 1);
-		if (pivot == -1)
+		if (nums == null || nums.length == 0)
+			return -1;
+		if (nums.length == 1) {
+			return nums[0] == target ? 0 : -1;
+		}
+		if (nums[nums.length - 1] > nums[0]) {
+			// its mean array is sorted not rotated
 			return search(nums, 0, nums.length - 1, target);
+		}
+		int pivot = pivot(nums, 0, nums.length - 1);
 		if (nums[pivot] == target)
 			return pivot;
 		if (target >= nums[0])
@@ -16,7 +23,7 @@ public class FindFromRotatedArray {
 	public static int search(int arr[], int first, int last, int target) {
 		if (first > last)
 			return -1;
-		int mid = (first + last) / 2;
+		int mid = first + (last - first) / 2;
 		if (arr[mid] == target)
 			return mid;
 		if (target >= arr[mid])
@@ -24,29 +31,23 @@ public class FindFromRotatedArray {
 		return search(arr, first, mid - 1, target);
 	}
 
+	// [5,1,2,3]
+	// [5,6,7,2]
 	static int pivot(int arr[], int low, int high) {
-		// base cases
-		if (high < low)
-			return -1;
-		if (high == low)
-			if (low > 0)
-				return -1;
-			else
-				return low;
-
 		/* low + (high - low)/2; */
-		int mid = (low + high) / 2;
-		if (mid < high && arr[mid] > arr[mid + 1])
+		int mid = low + (high - low) / 2;
+		if (arr[mid] > arr[mid + 1])
 			return mid;
-		if (mid > low && arr[mid] < arr[mid - 1])
-			return (mid - 1);
+		if (mid > low && arr[mid - 1] > arr[mid])
+			return mid - 1;
+
 		if (arr[low] >= arr[mid])
 			return pivot(arr, low, mid - 1);
 		return pivot(arr, mid + 1, high);
 	}
 
 	public static void main(String[] args) {
-		System.out.println(search(new int[] { 3, 5, 1 }, 3));
+		System.out.println(search(new int[] { 4, 5, 1, 2, 3 }, 3));
 	}
 
 }
