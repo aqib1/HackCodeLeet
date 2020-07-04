cc09642c010c7d2f44c119cc3e2493d6c566b15e
Aqib Javed <aqibbutt3078@gmail.com>
2020-07-04 23:45:57 +0500

    revison BT to single linked list

--- Test-tasks/src/BineryTreeToSingleLinkedList.java
+++ Test-tasks/src/BineryTreeToSingleLinkedList.java
@@ -1,8 +1,22 @@
 public class BineryTreeToSingleLinkedList {
 
+	private TreeNode previous = null;
+
+	// O(n) and space O(1)
 	public void flatten(TreeNode root) {
 		if (root == null)
 			return;
+		TreeNode left = root.left;
+		TreeNode right = root.right;
+
+		if (previous != null) {
+			previous.right = root;
+			previous.left = null;
+		}
+		previous = root;
+
+		flatten(left);
+		flatten(right);
 
 	}
 }
