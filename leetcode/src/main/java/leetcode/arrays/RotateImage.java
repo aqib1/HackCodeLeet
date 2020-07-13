b26ba1e4476d9fc37d6cb8fa6913d989801208db
Aqib Javed <aqibbutt3078@gmail.com>
2020-07-14 04:36:51 +0500

    update space complexity

--- leetcode/src/main/java/leetcode/arrays/RotateImage.java
+++ leetcode/src/main/java/leetcode/arrays/RotateImage.java
@@ -4,14 +4,14 @@ import java.util.Objects;
 
 public class RotateImage {
 
-	// Time complexity O(n) and space complexity O(1)
+	// Time complexity O(n) and space complexity O(n)
 	public int[][] rotateImage(int[][] image) {
 		if (Objects.isNull(image))
 			throw new IllegalArgumentException();
 		if (image.length == 0)
 			return new int[][] {};
 		int rotateYLimit = 2;
-		int [][] rotatedImage = new int[image.length][image[0].length];
+		int[][] rotatedImage = new int[image.length][image[0].length];
 		for (int x = 0; x < image.length; x++) {
 			for (int y = 0; y < image[x].length; y++) {
 				rotatedImage[y][rotateYLimit] = image[x][y];
