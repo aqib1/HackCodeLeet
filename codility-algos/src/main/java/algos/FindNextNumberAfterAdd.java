cf086010e7847184a0aad3754836dd22f84336f2
Aqib Javed <aqibbutt3078@gmail.com>
2020-07-16 00:27:31 +0500

    find next number after adding 1

---
+++ codility-algos/src/main/java/algos/FindNextNumberAfterAdd.java
@@ -0,0 +1,27 @@
+package algos;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class FindNextNumberAfterAdd {
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(returnAfterAdd(new int[] { 1, 9, 9 })));
+	}
+
+	private static Integer[] returnAfterAdd(int[] arr) {
+		if (arr == null || arr.length == 0) {
+			return null;
+		}
+		arr[arr.length - 1]++;
+		LinkedList<Integer> stack = new LinkedList<>();
+		int carry = 0;
+		for (int x = arr.length - 1; x >= 0; x--) {
+			arr[x] = arr[x] + carry;
+			int value = (arr[x] / 10 == 0 ? arr[x] : arr[x] % 10);
+			carry = arr[x] / 10;
+			stack.push(value);
+		}
+
+		return stack.toArray(new Integer[stack.size()]);
+	}
+}
