0316f373ee95b546048d29722e852cd725ee8cee
Aqib Javed <aqibbutt3078@gmail.com>
2020-07-24 06:03:11 +0500

    Amazon 2019 OA mock

---
+++ Time-Algoritms/src/main/java/algorithm/PackageLoaderALgorithm.java
@@ -0,0 +1,68 @@
+package algorithm;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class PackageLoaderALgorithm {
+	private static final int MARGIN = 30;
+
+	static class Package {
+		private int[] indexes;
+
+		private int[] values;
+
+		public Package() {
+			this(null, null);
+		}
+
+		public Package(int[] indexes, int[] values) {
+			this.indexes = indexes;
+			this.values = values;
+		}
+
+		public int[] getIndexes() {
+			return indexes;
+		}
+
+		public void setIndexes(int[] indexes) {
+			this.indexes = indexes;
+		}
+
+		public int[] getValues() {
+			return values;
+		}
+
+		public void setValues(int[] values) {
+			this.values = values;
+		}
+
+	}
+
+	// Time complexity O(n)2 and OlogN over all will be O(n)2 and space will be O(n)
+	public static int[] loadPackage(int packages[], int space) {
+		// Max heap (PriorityQueue default behavior is min heap so we will apply reverse
+		// comparator)
+		PriorityQueue<Package> max_heap = new PriorityQueue<>((p1, p2) -> {
+			int v1Comp = Integer.compare(p1.getValues()[0], p2.getValues()[0]);
+			int v2Comp = Integer.compare(p1.getValues()[1], p2.getValues()[1]);
+			if (v1Comp > 1 || v2Comp > 1 || v1Comp == 0 && v2Comp == 0)
+				return -1;
+
+			return 1;
+		});
+		for (int x = 0; x < packages.length; x++) {
+			for (int y = x + 1; y < packages.length; y++) {
+				int weight = packages[x] + packages[y];
+				if (weight == (space - MARGIN)) {
+					max_heap.add(new Package(new int[] { x, y }, new int[] { packages[x], packages[y] }));
+				}
+			}
+		}
+
+		return max_heap.isEmpty() ? new int[] {} : max_heap.poll().getIndexes();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(loadPackage(new int[] { 60, 60, 60, 60, 60 }, 90)));
+	}
+}
---
+++ Time-Algoritms/src/main/java/algorithm/ShippingRouteOptimal.java
@@ -0,0 +1,73 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class ShippingRouteOptimal {
+	static class Route {
+		private int distance;
+		private int[] routes;
+
+		private Route() {
+		}
+
+		private Route(int distance, int[] routes) {
+			this.distance = distance;
+			this.routes = routes;
+		}
+
+		public int getDistance() {
+			return distance;
+		}
+
+		public void setDistance(int distance) {
+			this.distance = distance;
+		}
+
+		public int[] getRoutes() {
+			return routes;
+		}
+
+		public void setRoutes(int[] routes) {
+			this.routes = routes;
+		}
+
+	}
+
+	// Time complexity is O(n)2 and OLog(n) over all O(n)2 and space complexity is
+	// O(n)
+	public static List<int[]> getRoutesOpt(int[][] forwardRoutes, int[][] backwardRoutes, int maxTravelDist) {
+		// Max heap (PriorityQueue default behavior is min heap so we will apply reverse
+		// comparator)
+		PriorityQueue<Route> max_heap = new PriorityQueue<>(
+				(r1, r2) -> Integer.compare(r2.getDistance(), r1.getDistance()));
+		List<int[]> result = new ArrayList<int[]>();
+		int max_dist = Integer.MIN_VALUE;
+		for (int f = 0; f < forwardRoutes.length; f++) {
+			for (int b = 0; b < backwardRoutes.length; b++) {
+				int sumDist = forwardRoutes[f][1] + backwardRoutes[b][1];
+				if (sumDist <= maxTravelDist)
+					max_heap.add(new Route(sumDist, new int[] { forwardRoutes[f][0], backwardRoutes[b][0] }));
+			}
+		}
+
+		while (!max_heap.isEmpty()) {
+			Route route = max_heap.poll();
+			if (max_dist <= route.distance) {
+				result.add(route.getRoutes());
+				max_dist = route.distance;
+			}
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		List<int[]> result = getRoutesOpt(new int[][] { { 1, 3000 }, { 2, 5000 }, { 3, 7000 }, { 4, 10000 } },
+				new int[][] { { 1, 2000 }, { 2, 3000 }, { 3, 4000 }, { 4, 5000 } }, 10000);
+		for (int[] r : result)
+			System.out.println(Arrays.toString(r));
+	}
+}
