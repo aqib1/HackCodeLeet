d7b134bb0af89c22e72931c03ccc2cf1559e2008
Aqib Javed <aqibbutt3078@gmail.com>
2020-08-04 15:47:54 +0500

    Tag Container map approach

---
+++ Time-Algoritms/src/main/java/algorithm/Amazon2k20_TagContainer_Map_Approach.java
@@ -0,0 +1,24 @@
+package algorithm;
+
+import java.util.List;
+
+public class Amazon2k20_TagContainer_Map_Approach {
+	public static class PairString {
+		private String first;
+		private String second;
+
+		public PairString(String first, String second) {
+			this.first = first;
+			this.second = second;
+		}
+
+		@Override
+		public String toString() {
+			return first + " ," + second;
+		}
+	}
+
+	public static List<String> largestItemAssociation(List<PairString> pairs) {
+
+	}
+}
