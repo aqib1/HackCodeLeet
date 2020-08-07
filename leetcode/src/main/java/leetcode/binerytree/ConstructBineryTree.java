dea75224edb2e723872b660fee2dff1fd3b43b3a
Aqib Javed <aqibbutt3078@gmail.com>
2020-08-07 05:10:22 +0500

    Construct Binery tree from PreOrder and InOrder

--- leetcode/src/main/java/leetcode/binerytree/ConstructBineryTree.java
+++ leetcode/src/main/java/leetcode/binerytree/ConstructBineryTree.java
@@ -5,11 +5,24 @@ import java.util.Map;
 
 public class ConstructBineryTree {
 	private Map<Integer, Integer> inOrderToIndexMap = new HashMap<>();
+	private int preorderIndex;
 
 	public TreeNode buildTree(int[] preorder, int[] inorder) {
 		for (int x = 0; x < inorder.length; x++)
 			inOrderToIndexMap.put(inorder[x], x);
 
-		return null;
+		return buildTree(preorder, inorder, 0, inorder.length - 1);
+	}
+
+	private TreeNode buildTree(int[] preorder, int[] inorder, int start, int end) {
+		if (start > end)
+			return null;
+		TreeNode node = new TreeNode(preorder[preorderIndex++]);
+		if (start == end)
+			return node;
+		int inOrderFound = inOrderToIndexMap.get(node.val);
+		node.left = buildTree(preorder, inorder, start, inOrderFound - 1);
+		node.right = buildTree(preorder, inorder, inOrderFound + 1, end);
+		return node;
 	}
 }
