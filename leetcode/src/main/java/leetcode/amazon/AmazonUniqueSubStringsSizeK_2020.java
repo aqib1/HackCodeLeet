afc08178689f37af642523facfccb7a999f719bc
Aqib Javed <aqibbutt3078@gmail.com>
2020-08-31 06:15:45 +0500

    init amazon unique sub string size k 2020

---
+++ leetcode/src/main/java/leetcode/amazon/AmazonUniqueSubStringsSizeK_2020.java
@@ -0,0 +1,29 @@
+package leetcode.amazon;
+
+import java.util.Set;
+
+public class AmazonUniqueSubStringsSizeK_2020 {
+
+	/**
+	 * Given a string s and an int k, return all unique substrings of s of size k
+	 * with k distinct characters.
+	 * 
+	 * Example 1:
+	 * 
+	 * Input: s = "abcabc", k = 3 Output: ["abc", "bca", "cab"] Example 2:
+	 * 
+	 * Input: s = "abacab", k = 3 Output: ["bac", "cab"] Example 3:
+	 * 
+	 * Input: s = "awaglknagawunagwkwagl", k = 4 Output: ["wagl", "aglk", "glkn",
+	 * "lkna", "knag", "gawu", "awun", "wuna", "unag", "nagw", "agwk", "kwag"]
+	 * Explanation: Substrings in order are: "wagl", "aglk", "glkn", "lkna", "knag",
+	 * "gawu", "awun", "wuna", "unag", "nagw", "agwk", "kwag", "wagl" "wagl" is
+	 * repeated twice, but is included in the output once. Constraints:
+	 * 
+	 * The input string consists of only lowercase English letters [a-z] 0 ≤ k ≤ 26
+	 */
+	public static Set<String> uniqueSubstringSizeK(String s, int k) {
+
+		return null;
+	}
+}
