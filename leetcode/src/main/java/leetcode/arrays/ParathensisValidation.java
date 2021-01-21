dfda4b0afe7264c8c482b76ba3225ed0ccf22926
DESKTOP-U7ONH0K\Aqib <aqibbutt3078@gmail.com>
2021-01-21 07:49:57 +0500

    Update ParathensisValidation.java

--- leetcode/src/main/java/leetcode/arrays/ParathensisValidation.java
+++ leetcode/src/main/java/leetcode/arrays/ParathensisValidation.java
@@ -1,5 +1,24 @@
 package leetcode.arrays;
 
-public class ParathensisValidation {
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
 
+public class ParathensisValidation {
+	public static boolean isValid(String paran) {
+		Stack<Character> stack = new Stack<>();
+		Set<Character> opening = new HashSet<>(Arrays.asList('{', '(', '['));
+		Map<Character, Character> closingRule = Map.of('}', '{', ')', '(', ']', '[');
+		for (int x = 0; x < paran.length(); x++) {
+			char current = paran.charAt(x);
+				if(opening.contains(current)) {
+					stack.push(current);
+				} else if(stack.isEmpty() || stack.pop() != closingRule.get(current)) {
+						return false;
+					}
+		}
+		return stack.isEmpty();
+	}
 }
