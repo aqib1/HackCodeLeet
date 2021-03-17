bdcef84d340bbf20946b2e4a7c8a60cf6ec5e599
DESKTOP-U7ONH0K\Aqib <aqibbutt3078@gmail.com>
2021-03-17 01:58:56 +0100

    MinimumUniqueArraySumAlgo

---
+++ HeliosX/src/MinimumUniqueArraySum.java
@@ -0,0 +1,13 @@
+import java.util.Objects;
+
+public class MinimumUniqueArraySum {
+
+	public static int getMinimumUniqueSum(int [] arr) {
+		if(Objects.isNull(arr) || arr.length == 0)
+			return 0;
+		int sum = 0;
+		
+		
+		return sum;
+	}
+}
