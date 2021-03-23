9a7055385b838fc492c88361ae048decf8b558f5
DESKTOP-U7ONH0K\Aqib <aqibbutt3078@gmail.com>
2021-03-23 03:45:40 +0100

    Updated IPV4, and IPV6 regex with exceptional invalidity

--- HeliosX/src/Ipv4AndIpv6.java
+++ HeliosX/src/Ipv4AndIpv6.java
@@ -1,24 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
+enum IP {
+	IPv4, IPv6, Neither
+}
+
 public class Ipv4AndIpv6 {
-	
-	private static final String IPV6_REGEX = "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"; 
+
+	private static final String IPV6_REGEX = "^([0-9A-Fa-f]{0,4}:){2,7}([0-9A-Fa-f]{1,4}$|((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.|$)){4})$";
 	private static final String IPV4_REGEX = "\\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.|$)){4}\\b";
+	private static final String EXCEPTIONAL_INVALIDITY_IPV6 = "^([0-9A-Fa-f]{0,4}:){2,3}[0-9A-Fa-f]{0,4}::[0-9]{2}:[0-9]{4}$";
+
 	public static boolean validateIp(String ip, String ipType) {
-	    return Pattern.compile(ipType).matcher(ip).find();
+		return Pattern.compile(ipType, Pattern.CASE_INSENSITIVE).matcher(ip).find();
 	}
 
-	public static String identifyAddressPattern(String ip) {
-	    if (validateIp(ip, IPV4_REGEX)) {
-	        return "IPv4";
-	    } else if (validateIp(ip, IPV6_REGEX)) {
-	        return "IPv6";
-	    } else {
-	        return "None";
-	    }
+	public static String checkIp(String ip) {
+		if (validateIp(ip, EXCEPTIONAL_INVALIDITY_IPV6)) {
+			return IP.Neither.name();
+		} else if (validateIp(ip, IPV4_REGEX)) {
+			return IP.IPv4.name();
+		} else if (validateIp(ip, IPV6_REGEX)) {
+			return IP.IPv6.name();
+		} else {
+			return IP.Neither.name();
+		}
 	}
-	
+
+	public static List<String> checkIps(List<String> ips) {
+		System.out.println(ips);
+		if (Objects.isNull(ips) || ips.isEmpty()) {
+			return Arrays.asList();
+		}
+
+		List<String> result = new ArrayList<>();
+
+		for (String ip : ips) {
+			result.add(checkIp(ip));
+		}
+		return result;
+	}
+
 	public static void main(String[] args) {
-		System.out.println(identifyAddressPattern("69.89.31.226"));
+
 	}
 }
