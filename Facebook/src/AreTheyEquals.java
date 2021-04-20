ce7555fdfcb0803b0b2c6b2736f177f8287496ae
DESKTOP-U7ONH0K\Aqib <aqibbutt3078@gmail.com>
2021-04-21 00:28:36 +0200

    AreTheyEquals Array

--- Facebook/src/AreTheyEquals.java
+++ Facebook/src/AreTheyEquals.java
@@ -1,4 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class AreTheyEquals {
 
+	public static boolean areTheyEqual(int[] array_a, int[] array_b) {
+		if (array_a.length != array_b.length)
+			return Boolean.FALSE;
+		Map<Integer, Integer> countMap = new HashMap<>();
+
+		for (int i = 0; i < array_a.length; i++) {
+			updateMap(countMap, array_a[i], 1);
+			updateMap(countMap, array_b[i], -1);
+		}
+
+		long result = countMap.values().stream().filter(value -> value != 0).findFirst().orElse(Integer.MIN_VALUE);
+		return result == Integer.MIN_VALUE ? Boolean.TRUE : Boolean.FALSE;
+	}
+
+	private static void updateMap(Map<Integer, Integer> countMap, int key, int incr) {
+		if (countMap.containsKey(key)) {
+			countMap.put(key, countMap.get(key) + incr);
+		} else {
+			countMap.put(key, incr);
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(areTheyEqual(new int[] { 2, 1, 1 }, new int[] { 1, 2, 1 }));
+	}
 }
