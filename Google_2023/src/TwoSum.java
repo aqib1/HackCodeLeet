f7d062324ad20b07208efd9c85f4497446702ff2
Aqib Javed <aqiajave@amazon.co.uk>
2022-11-25 06:30:22 +0000

    NeetCode 2Sum

--- Google_2023/src/TwoSum.java
+++ Google_2023/src/TwoSum.java
@@ -1,6 +1,23 @@
 import java.util.*;
 
 public class TwoSum {
+
+    public static int[] twoSumLessSpace(int [] nums, int target) {
+        Map<Integer, Integer> indexByNum = new HashMap<>();
+        for (int x = 0; x < nums.length; x++) {
+            int sub = target - nums[x];
+            if(indexByNum.containsKey(sub)) {
+                return new int [] {indexByNum.get(sub), x};
+            } else {
+                indexByNum.put(nums[x], x);
+            }
+        }
+        return new int[] {};
+    }
+
+    // Time complexity is O(n) while space is O2(n)
+    // we do not need two data-structure to solve this problem
+
     public static int[] twoSum(int[] nums, int target) {
         Set<Integer> diffHolder = new HashSet<>();
         Map<Integer, Integer> indexByNum = new HashMap<>();
