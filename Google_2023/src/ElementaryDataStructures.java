483de96691c71eadc59b7bd7852078360626e94b
Aqib Javed <aqiajave@amazon.co.uk>
2022-11-27 00:56:14 +0000

    NeetCode median of two sorted array using O(min(Log N, Log M)) with space O(1)

--- Google_2023/src/ElementaryDataStructures.java
+++ Google_2023/src/ElementaryDataStructures.java
@@ -2,9 +2,13 @@ import java.util.Objects;
 
 public class ElementaryDataStructures {
 
+
+//    [2, 3, 6] [1, 7, 10, 11]
+
+
     public static void main(String[] args) {
-        System.out.println(findByBinarySearchRecursive(new int[] {1, 2, 3, 4},
-                2));
+        System.out.println(findMedianSortedArraysEffective(new int[]{1, 2},
+                new int[]{3, 4}));
     }
 
     public static int findByBinarySearchRecursive(int[] data, int value) {
@@ -75,6 +79,62 @@ public class ElementaryDataStructures {
         int[] merged = merge(nums1, nums2);
         int halfIndex = merged.length / 2;
         return merged.length % 2 == 0 ? ((double) (merged[halfIndex - 1] + merged[halfIndex]) / 2) : (merged[halfIndex]);
+    }
+
+
+    // Time complexity O(log min(x,y))
+    public static double findMedianSortedArraysEffective(int[] nums1, int[] nums2) {
+        // make sure nums1 always small array
+        if (nums1.length > nums2.length) {
+            int[] tmp = nums2;
+            nums2 = nums1;
+            nums1 = tmp;
+        }
+
+        int startOfSmallArray = 0;
+        int endOfSmallArray = nums1.length;
+        int partitionOfSmallArray;
+        int partitionOfLargeArray;
+
+        while (startOfSmallArray <= endOfSmallArray) {
+            partitionOfSmallArray = (startOfSmallArray + endOfSmallArray) / 2;
+            partitionOfLargeArray = ((nums1.length + nums2.length + 1) / 2) - partitionOfSmallArray;
+
+            if (partitionOfSmallArray > 0
+                    && partitionOfLargeArray < nums2.length
+                    && nums1[partitionOfSmallArray - 1] > nums2[partitionOfLargeArray]) {
+                endOfSmallArray = partitionOfSmallArray - 1;
+            } else if (partitionOfLargeArray > 0
+                    && startOfSmallArray < nums1.length
+                    && nums2[partitionOfLargeArray - 1] > nums1[partitionOfSmallArray]) {
+                startOfSmallArray = partitionOfSmallArray + 1;
+            } else {
+                int total = nums1.length + nums2.length;
+                int maxLeft = Integer.MIN_VALUE;
+
+                if (partitionOfSmallArray > 0) {
+                    maxLeft = Math.max(maxLeft, nums1[partitionOfSmallArray - 1]);
+                }
+                if (partitionOfLargeArray > 0) {
+                    maxLeft = Math.max(maxLeft, nums2[partitionOfLargeArray - 1]);
+                }
+
+                if (total % 2 != 0)
+                    return maxLeft;
+                else {
+                    int minRight = Integer.MAX_VALUE;
+                    if (partitionOfSmallArray < nums1.length) {
+                        minRight = Math.min(minRight, nums1[partitionOfSmallArray]);
+                    }
+                    if (partitionOfLargeArray < nums2.length) {
+                        minRight = Math.min(minRight, nums2[partitionOfLargeArray]);
+                    }
+                    return (minRight + maxLeft) / 2.0;
+                }
+            }
+        }
+
+        return -1;
     }
 
 }
