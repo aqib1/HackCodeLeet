3964a9c1aa6f912936db6821b1194b0daafc412c
Aqib Javed <aqiajave@amazon.co.uk>
2022-11-28 05:30:17 +0000

    NeetCode median of two sorted array using O(min(Log N, Log M)) with space O(1)

--- Google_2023/src/ElementaryDataStructures.java
+++ Google_2023/src/ElementaryDataStructures.java
@@ -3,11 +3,18 @@ import java.util.Objects;
 public class ElementaryDataStructures {
 
 
-//    [2, 3, 6] [1, 7, 10, 11]
-
-
     public static void main(String[] args) {
-        System.out.println(findMedianSortedArraysEffective(new int[]{1, 2},
+//        int[] evenSizedArray = new int[]{1, 2, 3, 4};
+//
+//        int mid = evenSizedArray.length / 2;
+//        double median = (double)(evenSizedArray[mid - 1] + evenSizedArray[mid]) / 2.0;
+//        System.out.println(median);
+//
+//        int[] oddSizedArray = new int[] {1, 2, 3};
+//        System.out.println(oddSizedArray[oddSizedArray.length / 2]);
+
+
+        System.out.println(median(new int[]{1, 2},
                 new int[]{3, 4}));
     }
 
@@ -61,15 +68,12 @@ public class ElementaryDataStructures {
                 merge[x++] = nums2[j++];
             }
         }
-
         while (i < nums1.length) {
             merge[x++] = nums1[i++];
         }
-
         while (j < nums2.length) {
             merge[x++] = nums2[j++];
         }
-
         return merge;
     }
 
@@ -81,6 +85,56 @@ public class ElementaryDataStructures {
         return merged.length % 2 == 0 ? ((double) (merged[halfIndex - 1] + merged[halfIndex]) / 2) : (merged[halfIndex]);
     }
 
+
+    public static double median(int X[], int Y[]) {
+        // step 1: check if X > Y then replace
+        if (X.length > Y.length) {
+            int[] tmp = Y;
+            Y = X;
+            X = tmp;
+        }
+        int startOfSmallArray = 0;
+        int endOfSmallArray = X.length;
+        int partitionOfSmallArray;
+        int partitionOfLargeArray;
+        boolean isOdd = (X.length + Y.length) % 2 != 0;
+
+        while (startOfSmallArray <= endOfSmallArray) {
+            partitionOfSmallArray = (startOfSmallArray + endOfSmallArray) / 2;
+            partitionOfLargeArray = ((X.length + Y.length + 1) / 2) - partitionOfSmallArray;
+            // x0 > y1
+            if (partitionOfSmallArray > 0 && partitionOfLargeArray < Y.length
+                    && X[partitionOfSmallArray - 1] > Y[partitionOfLargeArray]) {
+                endOfSmallArray = partitionOfSmallArray - 1;
+            }
+            // x1 < y0
+            else if (partitionOfSmallArray < X.length && partitionOfLargeArray > 0
+                    && X[partitionOfSmallArray] < Y[partitionOfLargeArray - 1]) {
+                startOfSmallArray = partitionOfSmallArray + 1;
+            } else {
+                int maxLeft = Integer.MIN_VALUE;
+                if (partitionOfSmallArray > 0) {
+                    maxLeft = Math.max(maxLeft, X[partitionOfSmallArray - 1]);
+                }
+                if (partitionOfLargeArray > 0) {
+                    maxLeft = Math.max(maxLeft, Y[partitionOfLargeArray - 1]);
+                }
+                if (isOdd) return maxLeft;
+                else {
+                    int minRight = Integer.MAX_VALUE;
+                    if (partitionOfSmallArray < X.length) {
+                        minRight = Math.min(minRight, X[partitionOfSmallArray]);
+                    }
+                    if (partitionOfLargeArray < Y.length) {
+                        minRight = Math.min(minRight, Y[partitionOfLargeArray]);
+                    }
+                    return (maxLeft + minRight) / 2.0;
+                }
+            }
+        }
+
+        return -1;
+    }
 
     // Time complexity O(log min(x,y))
     public static double findMedianSortedArraysEffective(int[] nums1, int[] nums2) {
