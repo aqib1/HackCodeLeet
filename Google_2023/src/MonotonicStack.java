4a5052d53fdd1d77868fa2f5cdb8fad74833d512
Aqib Javed <aqiajave@amazon.co.uk>
2022-12-01 08:23:10 +0000

    Leetcode, next greater II problem which explain monotonic statcks

--- Google_2023/src/MonotonicStack.java
+++ Google_2023/src/MonotonicStack.java
@@ -1,20 +1,54 @@
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.Stack;
+import java.util.stream.IntStream;
 
 public class MonotonicStack {
 
+
+    // Leetcode : https://leetcode.com/problems/next-greater-element-ii/description/
+    // Approach Monotonic stacks
+    // Time complexity is O(N x M) where M is the number of elements,
+    // stack will remove while maintaining order in worst case.
+    // Space complexity O(N)
+    public static int[] nextGreaterElements(int[] nums) {
+        if (Objects.isNull(nums) || nums.length == 0) return null;
+
+        Stack<Integer> monotonicStack = new Stack<>();
+        var result = new int[nums.length];
+        int limit = nums.length;
+        for (int last = (limit - 1) * 2; last >= 0; last--) {
+            while (!monotonicStack.isEmpty()
+                    && nums[last%limit] >= monotonicStack.peek()) {
+                monotonicStack.pop();
+            }
+
+            if (monotonicStack.isEmpty()) {
+                result[last%limit] = -1;
+            } else {
+                result[last%limit] = monotonicStack.peek();
+            }
+            monotonicStack.push(nums[last%limit]);
+        }
+        return result;
+    }
+
+
+    // Time complexity O(N x M) in worst case, where M are number
+    // of elements need to removed when nums2[M] >= monotonicStack.peek()
+    // Space O(N)
     public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
         var numberByNextGreater = new HashMap<>();
         var result = new int[nums1.length];
         // order in descending
         Stack<Integer> monotonicStack = new Stack<>();
         // traverse array from back-word
-        for(int i = nums2.length - 1; i >= 0; i--) {
-            while(!monotonicStack.isEmpty() && nums2[i] >= monotonicStack.peek()) {
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!monotonicStack.isEmpty() && nums2[i] >= monotonicStack.peek()) {
                 monotonicStack.pop();
             }
-            if(monotonicStack.isEmpty()) {
+            if (monotonicStack.isEmpty()) {
                 numberByNextGreater.put(nums2[i], -1);
             } else {
                 numberByNextGreater.put(nums2[i], monotonicStack.peek());
@@ -22,15 +56,15 @@ public class MonotonicStack {
             monotonicStack.push(nums2[i]);
         }
         int i = 0;
-        for(int val: nums1) {
-            result[i++] = (int)numberByNextGreater.get(val);
+        for (int val : nums1) {
+            result[i++] = (int) numberByNextGreater.get(val);
         }
         return result;
     }
 
     public static void main(String[] args) {
         System.out.println(
-                Arrays.toString(nextGreaterElement(new int[] {4,1,2}, new int[] {1,3,4,2}))
+                Arrays.toString(nextGreaterElements(new int[]{5,4,3,2,1}))
         );
     }
 }
