6bbf0096d60b7013b35748965796c692a943b2a5
Aqib Javed <aqiajave@amazon.co.uk>
2022-12-08 23:14:01 +0000

    BinaryTree: isSame Tree

---
+++ Google_2023/src/IsSameTree.java
@@ -0,0 +1,9 @@
+public class IsSameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+
+        if(p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
