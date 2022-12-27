d36df8dccba2c4fa91f4f36708b30e32603e0915
Aqib Javed <aqiajave@amazon.co.uk>
2022-12-27 15:48:22 +0000

    Leetcode: Binery search of sorted array list

--- Google_2023/src/BinarySearch.java
+++ Google_2023/src/BinarySearch.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class BinarySearch {
+public class BinarySearch {
+    private int[] nums;
+    public int search(int[] nums, int target) {
+        this.nums = nums;
+        return bs(0, this.nums.length - 1, target);
+    }
+    public int bs(int start, int end, int target) {
+        if(start > end) return -1;
+        int mid = (start + end) / 2;
+        if(this.nums[mid] == target) return mid;
+
+        if(target > this.nums[mid]) {
+            return bs(mid + 1, end, target);
+        } else {
+            return bs(start, mid - 1, target);
+        }
+    }
 }
