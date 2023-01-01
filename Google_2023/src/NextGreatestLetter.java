1059101387bf02c3a47dc909d02a6c6f13946b49
Aqib Javed <aqiajave@amazon.co.uk>
2023-01-01 17:10:26 +0000

    Leetcode: NextGreatestLetter algorithms

---
+++ Google_2023/src/NextGreatestLetter.java
@@ -0,0 +1,32 @@
+public class NextGreatestLetter {
+
+
+    // Time complexity O(N)
+    // Space complexity O(1)
+    public char nextGreatestLetter(char[] letters, char target) {
+
+        for (char letter : letters) {
+            if (letter > target)
+                return letter;
+        }
+
+        return letters[0];
+    }
+
+    public char nextGreatestLetterOptimal(char [] letters, char target) {
+        int start = 0, end = letters.length;
+
+        while(start < end) {
+            int mid = (start + end) / 2;
+            if(letters[mid] <= target) start = mid + 1;
+            else end = mid;
+        }
+
+        return letters[start % letters.length];
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new NextGreatestLetter().nextGreatestLetter(new char[] {'e', 'e', 'g', 'g'}, 'g'));
+    }
+}
