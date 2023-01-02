ded1a8c29dc945837a0566033dc33ae81b5e450a
Aqib Javed <aqiajave@amazon.co.uk>
2023-01-02 03:05:22 +0000

    Leetcode: GuessGame

---
+++ Google_2023/src/GuessNumberHigherOrLower.java
@@ -0,0 +1,31 @@
+import java.util.stream.IntStream;
+
+interface GuessGame {
+    int guess(int num);
+}
+public class GuessNumberHigherOrLower implements GuessGame {
+
+    public int guessNumber(int n) {
+        if(n == 1) return 1;
+
+        int start = 0, end = n;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            int result = guess(mid);
+            if(result == 0) return mid;
+            if(result == 1) start = mid + 1;
+            else end = mid - 1;
+        }
+        return -1;
+    }
+
+    @Override
+    public int guess(int num) {
+        return Integer.compare(2126753390, num);
+    }
+
+    public static void main(String[] args) {
+        GuessNumberHigherOrLower i = new GuessNumberHigherOrLower();
+        System.out.println(i.guessNumber(2126753390));
+    }
+}
