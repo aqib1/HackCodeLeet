b09e2f59a07d1405eb435e8962ef40f5c84b7548
Aqib Javed <aqiajave@amazon.co.uk>
2023-01-05 23:47:45 +0000

    Leetcode: remove duplicates without using extra space

---
+++ Google_2023/src/RemoveDuplicates.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Objects;
+
+public class RemoveDuplicates {
+
+    public static int removeDuplicates(int[] nums) {
+        if(Objects.isNull(nums) || nums.length == 0) return 0;
+        if(nums.length == 1) return 1;
+
+        int k = 1, match = 0;
+        for(int start = 0; start < nums.length - 1; start++) {
+            if(nums[start] == nums[start + 1]) {
+                match++;
+            } else {
+                nums[k] = nums[match + 1];
+                match = start + 1;
+                k++;
+
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(new int[] {1, 1, 2}));
+    }
+
+}
