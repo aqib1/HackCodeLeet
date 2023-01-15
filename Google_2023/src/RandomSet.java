7a860b4571531c9e4e81404a66f94502f433653a
Aqib Javed <aqibbutt3078@gmail.com>
2023-01-15 10:50:12 +0000

    Leetcode: RandomSet using O(1)

--- Google_2023/src/RandomSet.java
+++ Google_2023/src/RandomSet.java
@@ -1,20 +1,17 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class RandomSet {
-    // O(N)
+    // Space O(N)
+    // Time complexity, smartly O(1) with a very interesting trick
     private final Map<Integer, Integer> map;
     private final List<Integer> list;
     private int currentIndex;
-    private int randomIndex = 0;
+    private static final Random RAND = new Random();
     public RandomSet() {
-        map = new HashMap<>();
+        this.map = new HashMap<>();
         list = new ArrayList<>();
     }
 
-    // O(1)
-    // map = [[0, 1], [1, 2], [2, 3]]
-    // reverseValueIndex = [[1, 0], [2, 1], [3, 2]]
     public boolean add(int val) {
         if(map.containsKey(val)) return false;
         map.put(val, currentIndex);
@@ -25,32 +22,28 @@ public class RandomSet {
 
     public boolean delete(int val) {
         if(!map.containsKey(val)) return false;
-        int valueIndex = map.remove(val);
+        int valueIndex = map.get(val);
         list.set(valueIndex, list.get(list.size() - 1));
         map.put(list.get(list.size() - 1), valueIndex);
         list.remove(list.size() - 1);
+        map.remove(val);
         currentIndex--;
         return true;
     }
 
-    // 1, 2, 3, 4
-    // map = [[0, 1],  ,[2, 3]]
-    // reverseValueIndex = [[1, 0], [2, 1], [3, 2]]
     public int getRandom() {
-        int res = this.list.get(randomIndex);
-        randomIndex++;
-        if(randomIndex >= this.list.size()) randomIndex = 0;
-        return res;
+        return this.list.get(RAND.nextInt(list.size()));
     }
 
     public static void main(String[] args) {
         RandomSet randomSet = new RandomSet();
-        randomSet.add(0);
-        randomSet.add(1);
-        randomSet.delete(0);
-        randomSet.add(2);
-        randomSet.delete(1);
         System.out.println(randomSet.getRandom());
+        System.out.println(randomSet.delete(0));
+        System.out.println(randomSet.delete(0));
+        System.out.println(randomSet.add(0));
+        System.out.println(randomSet.getRandom());
+        System.out.println(randomSet.delete(0));
+        System.out.println(randomSet.add(0));
 
 //        randomSet.add(5);
 //        randomSet.add(6);
