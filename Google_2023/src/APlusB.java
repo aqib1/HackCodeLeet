a729f2b1be81e1d537c98894acb904fe78221f75
Aqib Javed <aqibbutt3078@gmail.com>
2023-01-18 10:50:12 +0000

    Testing

--- Google_2023/src/APlusB.java
+++ Google_2023/src/APlusB.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 
 public class APlusB {
-
+   static int add (int a, int b) {
+       return a + b;
+   }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(scanner.nextInt() + scanner.nextInt());
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(add(a, b));
         scanner.close();
     }
 }
